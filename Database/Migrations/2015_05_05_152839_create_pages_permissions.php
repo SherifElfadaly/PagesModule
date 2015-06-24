55cddf7960b08b8b76397d4dc5f9d27e87bcb5d5
SherifAshraf <sherif.marvel@outlook.com>
2015-06-24 14:19:06 +0200

    Changes

--- Database/Migrations/2015_05_05_152839_create_pages_permissions.php
+++ Database/Migrations/2015_05_05_152839_create_pages_permissions.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateContentPermissions extends Migration
+class CreatePagesPermissions extends Migration
 {
 	/**
 	 * Run the migrations.
